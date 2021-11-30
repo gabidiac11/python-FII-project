4c5429098f5a5ae6bb0e7dc764c4dbabfd961871
Gabi <gabidiac11@gmail.com>
2021-11-30 14:01:50 +0200

    handled exeptions; added functions for creating folder/file; updated verbose

--- main.py
+++ main.py
@@ -22,7 +22,6 @@
 #TODO:
 # - read from file
 # - get main arguments from console
-# - handle exeptions
 # - test on linux when fix are done
 
 import json
@@ -32,13 +31,22 @@ def getJsonText(pathJson):
     return '{"dir1" : {"dir2": {"file1": "continut1", "file2": "continut2"}, "file3": "continut3"}, "file4": "continut4"}'
 
 def createFile(path, content):
-    print("Created file " + path + ", with content '" + content + "'")
+    f = open(path, 'w+')
+    f.write(content)
+    f.close()
+
+    print("Created/overwritten file " + path + ", with content '" + content + "'")
 
 def createFolder(path):
-    if os.path.exists(path):
-        print("Folder exists: " + path)
+    if os.path.exists(path) and os.path.isdir(path):
+        print("Folder already exists: " + path)
         return
 
+    if os.path.exists(path) and not(os.path.isdir(path)):
+        os.remove(path)
+        print("Folder name is already used for a non-folder... so the respective entity was deleted: " + path)
+
+    os.mkdir(path)
     print("Folder created " + path)
 
 def createStructure(root, obj):
@@ -55,11 +63,14 @@ def main(root, pathJson):
     obj = json.loads(getJsonText(pathJson))
     createStructure(root, obj)
 
-root = "."
+root = "./output"
 pathJson = "./"
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main(root, pathJson)
+    try:
+        main(root, pathJson)
+    except Exception as e:
+        print(e)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
